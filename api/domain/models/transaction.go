a5d4394bf799c1268a7512869594e8da059bb5d7
Alejandro Herrera <alejandro.herrera@pedidosya.com>
2020-08-22 15:23:57 -0300

    add transaction type

--- api/domain/models/transaction.go
+++ api/domain/models/transaction.go
@@ -1,4 +1,12 @@
 package models
 
+type TransactionType string
+
+const (
+	TransactionTypeDebit  TransactionType = "debit"
+	TransactionTypeCredit TransactionType = "credit"
+)
+
 type Transaction struct {
+	TransactionType TransactionType
 }
